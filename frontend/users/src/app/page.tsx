f9cb76cff901108bc5c8d27c00f98b66e94296bf
Alejo Borracci <borraccialejo@hotmail.es>
2024-07-29 12:19:26 -0300

    styles

--- frontend/users/src/app/page.tsx
+++ frontend/users/src/app/page.tsx
@@ -1,23 +1,23 @@
 export default function Login() {
   return (
-    <main>
-      <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh] bg-slate-700">
-        <div className="w-[50%] h-[70%] bg-black">
-          <form className="flex flex-col items-center">
-            <label htmlFor="name">
-              <input></input>
+    <main className="h-[100vh]">
+      <div className="flex flex-col items-center justify-center w-full h-full bg-slate-700">
+        <div className="flex w-[50%] h-[70%] bg-black">
+          <form className="form">
+            <label className="label" htmlFor="name">
+              <input className="input" placeholder="Ingrese su nombre"></input>
             </label>
-            <label htmlFor="subname">
-              <input></input>
+            <label className="label" htmlFor="subname">
+              <input className="input" placeholder="Ingrese su apellido"></input>
             </label>
-            <label htmlFor="phone">
-              <input></input>
+            <label className="label" htmlFor="phone">
+              <input className="input" placeholder="Ingrese su numero telefonico"></input>
             </label>
-            <label htmlFor="location">
-              <input></input>
+            <label className="label" htmlFor="location">
+              <input className="input" placeholder="Ingrese su localizacion"></input>
             </label>
-            <label htmlFor="image">
-              <input></input>
+            <label className="label" htmlFor="image">
+              <input className="input" placeholder="Ingrese su imagen"></input>
             </label>
           </form>
         </div>
